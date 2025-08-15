4906de91fd673b805c230968f21b1ba79622c8aa
codeshaine <shainilps05@gmail.com>
2025-08-15 22:10:33 +0530

    udpate: telescope config:

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,10 @@ return {
 		config = function()
 			local builtin = require("telescope.builtin")
 
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
 			vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {})
 
