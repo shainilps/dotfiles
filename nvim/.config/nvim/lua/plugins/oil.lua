c5587a9ef914cf4152b06e83d00e2bf45365ad69
shainilps <shainilps.work@gmail.com>
2025-10-19 17:16:05 +0530

    disabled some keybindigs of plugin

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,12 @@
 return {
 	"stevearc/oil.nvim",
 	config = function()
-		require("oil").setup({})
+		require("oil").setup({
+			keymaps = {
+				["<C-l>"] = false,
+				["<C-h>"] = false,
+			},
+		})
 		vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
 }
