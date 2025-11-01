466b0ac3f114faa6368e0fb680c68a6375b8be25
shainilps <shainilps.work@gmail.com>
2025-11-01 20:30:46 +0530

    fixed some color issue

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -6,6 +6,10 @@ return {
 				["<C-l>"] = false,
 				["<C-h>"] = false,
 			},
+
+			view_options = {
+				show_hidden = true,
+			},
 		})
 		vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
