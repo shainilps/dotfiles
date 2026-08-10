18c3cfd9f65fa6eb84bb007a1adae673eede6dc3
shainilps <me@shainilps.com>
2026-08-10 16:41:24 +0530

    update: few update in the noice

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -5,12 +5,48 @@ return {
 	config = function()
 		require("noice").setup({
 			presets = { command_palette = true },
+
+			cmdline = {
+				format = {
+					filter = {
+						pattern = "^:%s*!",
+						icon = "$",
+						title = " Shell ",
+						lang = "shell",
+					},
+				},
+			},
+
 			lsp = { signature = { auto_open = { enabled = false } } },
-			views = { mini = { timeout = 2000, reverse = false } },
+			views = {
+				mini = { timeout = 2000, reverse = false },
+				shell_output = {
+					backend = "split",
+					position = "right",
+					size = "40%",
+					enter = true,
+					close = {
+						keys = { "q", "<Esc>" },
+					},
+					win_options = {
+						winhighlight = "Normal:Normal",
+					},
+				},
+			},
 			routes = {
 				{
 					filter = { event = "lsp", kind = "progress" },
 					opts = { skip = true },
+				},
+				{
+					view = "shell_output",
+					filter = {
+						event = "msg_show",
+						kind = {
+							"shell_out",
+							"shell_err",
+						},
+					},
 				},
 			},
 		})
