7fcc035ede01f3422bef34f30f81e193a213d8b9
shainilps <me@shainilps.com>
2026-08-10 21:52:20 +0530

    update: few update in the noice

--- nvim/.config/nvim/lua/plugins/leap.lua
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -1,9 +1,18 @@
 return {
-	url = "https://codeberg.org/andyg/leap.nvim",
+	"https://codeberg.org/andyg/leap.nvim",
 	event = "VeryLazy",
+
 	config = function()
-		vim.keymap.set({ "n", "x" }, "gs", "<Plug>(leap-forward)", { desc = "Leap forward" })
-		vim.keymap.set({ "n", "x" }, "gS", "<Plug>(leap-backward)", { desc = "Leap backward" })
-		vim.keymap.set({ "n", "x", "o" }, "gm", "<Plug>(leap-from-window)", { desc = "Leap to window" })
+		vim.keymap.set({ "n", "x", "o" }, "gs", "<Plug>(leap-forward)", {
+			desc = "Leap forward",
+		})
+
+		vim.keymap.set({ "n", "x", "o" }, "gS", "<Plug>(leap-backward)", {
+			desc = "Leap backward",
+		})
+
+		vim.keymap.set({ "n", "x", "o" }, "gm", "<Plug>(leap-from-window)", {
+			desc = "Leap from window",
+		})
 	end,
 }
