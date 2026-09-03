19944da4170f9d11a8034c0933689cbfd19ecc43
shainilps <me@shainilps.com>
2026-09-03 09:30:30 +0530

    some update to fugitive

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,6 +1,10 @@
 return {
 	"tpope/vim-fugitive",
+	dependencies = { "tpope/vim-rhubarb" },
 	config = function()
-		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "fugitive window" })
+		vim.keymap.set({ "n", "v" }, "<leader>gl", ":G log<CR>", { desc = "git logs" })
+		vim.keymap.set({ "n", "v" }, "<leader>go", ":GBrowse<CR>", { desc = "open in browser" })
+		vim.keymap.set({ "n", "v" }, "<leader>gb", ":G blame<CR>", { desc = "git blame" })
 	end,
 }
--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,16 +1,16 @@
 return {
-	"ThePrimeagen/harpoon",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local harpoon_ui = require("harpoon.ui")
-		local harppon_mark = require("harpoon.mark")
-
-		vim.keymap.set("n", "<leader>h", harpoon_ui.toggle_quick_menu)
-		vim.keymap.set("n", "<leader>a", harppon_mark.add_file)
-		for i = 1, 9 do
-			vim.keymap.set("n", "<leader>" .. i, function()
-				harpoon_ui.nav_file(i)
-			end, { noremap = true, silent = true })
-		end
-	end,
+	-- "ThePrimeagen/harpoon",
+	-- dependencies = { "nvim-lua/plenary.nvim" },
+	-- config = function()
+	-- 	local harpoon_ui = require("harpoon.ui")
+	-- 	local harppon_mark = require("harpoon.mark")
+	--
+	-- 	vim.keymap.set("n", "<leader>h", harpoon_ui.toggle_quick_menu)
+	-- 	vim.keymap.set("n", "<leader>a", harppon_mark.add_file)
+	-- 	for i = 1, 9 do
+	-- 		vim.keymap.set("n", "<leader>" .. i, function()
+	-- 			harpoon_ui.nav_file(i)
+	-- 		end, { noremap = true, silent = true })
+	-- 	end
+	-- end,
 }
